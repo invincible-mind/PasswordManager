6e38ce063347f62e51fce238daa84af0ce33e4e5
Thane <lindengill2004@gmail.com>
2025-04-29 10:54:52 -0400

    account and shared account
    Idk if this will work with how current user looks rn

--- src/PasswordManager/Account.java
+++ src/PasswordManager/Account.java
@@ -1,4 +1,47 @@
-package PasswordManager;
+package passwordManager;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Account {
+	private String username;
+	private String password;
+	private Date dateCreated;
+	private PasswordVerifier passwordVerifier;
+	private Map<Date,String> oldPasswords = new HashMap<>();
+
+
+	public Account(String username, String password, PasswordVerifier passwordVerifier) {
+		this.username = username;					// check in here for password validity?
+		this.passwordVerifier = passwordVerifier;
+		this.password = password;
+		this.dateCreated = 	new Date();
+	}
+	
+	public int update(String newPassword) {
+		if (passwordVerifier.checkPassword(newPassword)) {
+			return 0;
+		}
+		oldPasswords.put(dateCreated, password);
+		password = newPassword;
+		dateCreated = new Date();
+		return 1;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	
+	public Map<Date,String> getOldPasswords() {
+		return oldPasswords;
+	}
 }
---
+++ src/PasswordManager/SharedAccount.java
@@ -0,0 +1,47 @@
+package passwordManager;
+
+import java.util.Set;
+
+public class SharedAccount extends Account {
+	private Set<User> admins;
+	private Set<User> users;
+	
+	public SharedAccount(String username, String password, PasswordVerifier passwordVerifier, Set<User> admins, Set<User> users) {
+		super(username, password, passwordVerifier);
+		this.admins = admins;
+		this.users = users;
+	}
+	
+	public boolean addUser(User user) {
+		return users.add(user);
+	}
+	
+	public boolean addAdmin(User user) {
+		return admins.add(user);
+	}
+	
+	public boolean removeUser() {
+		return users.remove(user);
+	}
+	
+	public boolean removeAdmin() {
+		return admins.remove(user);
+	}
+	
+	public int verifyUser(User currentUser) {
+		if (admins.contains(currentUser)) {
+			return 1;
+		} else if (users.contains(currentUser)) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+	
+	public int update(String newPassword, User currentUser) {
+		if (verifyUser(currentUser) != 1) {
+			return -1;
+		}
+		super(newPassword);
+	}
+}
